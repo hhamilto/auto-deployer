e5ff148ccef4e0d90787c1e17e6d3012079ee1e4
Hurricane Hamilton <hhamilto@mtu.edu>
2016-10-08 16:49:23 -0500

    first stab at an install script

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ dependencies: `curl`, `apt-get`, and `bash` are installed.
 To begin, run this command:
 
     curl https://github.com/path/to/script
+
+
+# Limitations
+Only public projects supported right now. Future support should include generateing an ssh key automatically
--- self-install.sh
+++ self-install.sh
@@ -1,17 +1,48 @@
 #! /bin/bash
 
+BASEDIR=$(dirname $0)
+
 #install git and node
 
 sudo apt-get install git wget
 
-if
 wget https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-x64.tar.xz
 tar -zxvf node-v*.tar.xz
 
 echo 'export PATH=$PATH:/home/`whoami`/node-v6.7.0-linux-x64/bin' > .noninteractive_profile
 echo 'export PATH=$PATH:/home/`whoami`/node-v6.7.0-linux-x64/bin' > .bashrc
-
-# clone watch dog
+source .bashrc
 
 # clone actual application
 echo 'What is the git url of the repository to clone and run?'
+read REPO_URL
+NODE_PROJECT_DIRECTORY=`git clone $REPO_URL 2>&1 | grep 'Cloning into' | sed 's/.*'"'"'\(.*\)'"'"'.*/\1/' | xargs realpath`
+
+cd $NODE_PROJECT_DIRECTORY
+npm install
+cd $BASEDIR
+
+# clone watch dog
+git clone https://github.com/hhamilto/watchdog.git watchdog_for_project
+# configure watchdog
+echo "
+RESTART_COMMAND='bash -c \"cd $NODE_PROJECT_DIRECTORY; npm start\"'
+" > watchdog_for_project/conf.env
+
+watchdog_for_project/start-dog.sh # also starts project up
+
+# finally clone self and start listening for githooks
+
+git clone https://github.com/hhamilto/auto-deployer.git
+AUTO_DEPLOYER_DIRECTORY=realpath auto-deployer
+echo "$NODE_PROJECT_DIRECTORY" > auto-deployer/PROJECT_TO_PULL
+
+# give the githook listener a watchdog
+cp -r watchdog_for_project watchdog_for_hook_reciever
+
+# Start the hook reciever
+echo "
+RESTART_COMMAND='bash -c \"cd $AUTO_DEPLOYER_DIRECTORY; npm start\"'
+" > watchdog_for_hook_reciever/conf.env
+
+watchdog_for_hook_reciever/start-dog.sh # also starts project up
